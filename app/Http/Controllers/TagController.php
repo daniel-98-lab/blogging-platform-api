aa555103c49d0d52da179d0f050559b902c00de6
DanDev <danielcrd1216@gmail.com>
2025-01-02 19:26:58 +0100

    CRUD tags

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TagRequest;
+use App\Models\Tag;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tags = Tag::all();
+        return response()->json($tags);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+    */
+    public function store(TagRequest $request)
+    {
+            // Validate the open data
+            $tag = Tag::create($request->validated());
+
+            return response()->json([
+                'message' => 'Tag created successfully',
+                'tag'     => $tag
+            ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        // Get a specific tag
+        $tag = Tag::find($id);
+
+        if(!$tag) {
+            return response()->json(['message' => 'Tag not found'], 404);
+        }
+
+        return response()->json($tag);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(TagRequest $request, string $id)
+    {
+        // Get a specific tag
+        $tag = Tag::find($id);
+
+        if (!$tag) {
+            return response()->json(['message' => 'Tag not found'], 404);
+        }
+
+        // Update the tag
+        $tag->update($request->validated());
+
+        return response()->json([
+            'message' => 'Tag updated successfully',
+            'tag' => $tag
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        // Get a specific tag
+        $tag = Tag::find($id);
+
+        if (!$tag) {
+            return response()->json(['message' => 'Tag not found'], 404);
+        }
+
+        // Remove the tag
+        $tag->delete();
+
+        return response()->json(['message' => 'Tag deleted successfully'], 204);
+    }
+}
---
+++ app/Models/Tag.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    /**
+     * The table associated with the model
+     *
+     * @var string
+    */
+    protected $table = 'tags';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * The posts that belong to the tag.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class, 'posts_tags');
+    }
+}
